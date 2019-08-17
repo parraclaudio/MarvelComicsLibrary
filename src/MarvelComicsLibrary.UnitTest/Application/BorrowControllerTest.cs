73ec00ffb79054f6aaa6c1746e77880fc426fcbc
ClaudioParra <parra.claudio@hotmail.com>
2019-08-17 10:40:21 -0300

    test layer

---
+++ src/MarvelComicsLibrary.UnitTest/Application/BorrowControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using GenFu;
+using MarvelComicsLibrary.Application.Controllers;
+using MarvelComicsLibrary.Application.ViewModel;
+using MarvelComicsLibrary.Domain.Entity;
+using MarvelComicsLibrary.Service.Interface;
+using MarvelComicsLibrary.UnitTest.Config;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarvelComicsLibrary.UnitTest.Application
+{
+    public class BorrowControllerTest
+    {
+        private readonly Mock<IBorrowService> _service;
+        private readonly BorrowController _controller;
+
+        public BorrowControllerTest()
+        {
+            _service = new Mock<IBorrowService>();
+            _controller = new BorrowController(AutoMapperConfigTest.GetInstance(),_service.Object);
+        }
+
+        [Fact(DisplayName = "Post Success")]
+        public void PostSuccess()
+        {
+
+            var borrowViewModel = A.New<BorrowViewModel>();
+            var borrowEntity = A.New<Borrow>();
+
+            _service.Setup(x => x.Add(It.IsAny<Borrow>())).Returns(borrowEntity);
+
+            var result = _controller.Post(borrowViewModel);
+
+            Assert.NotNull(result);
+
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+    }
+}
--- src/MarvelComicsLibrary.UnitTest/Config/AutoMapperConfigTest.cs
+++ src/MarvelComicsLibrary.UnitTest/Config/AutoMapperConfigTest.cs
@@ -1,13 +1,12 @@
-﻿
-
-using AutoMapper;
+﻿using AutoMapper;
 using MarvelComicsLibrary.Application.AutoMapper;
 
-namespace MarvelComicsLibrary.Test.Config
+
+namespace MarvelComicsLibrary.UnitTest.Config
 {
     public class AutoMapperConfigTest
     {
-       public static IMapper GetInstance()
+        public static IMapper GetInstance()
         {
             var mapperConfig = AutoMapperConfig.Register();
             return mapperConfig.CreateMapper();
