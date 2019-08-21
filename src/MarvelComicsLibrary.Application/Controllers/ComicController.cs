59add6dc669ec53ad86f563f0b5deb23bb8a5bc6
Claudio Cesar Ruela Parra <claudio.parra@ype.ind.br>
2019-08-20 21:57:26 -0300

    Add comments

--- src/MarvelComicsLibrary.Application/Controllers/ComicController.cs
+++ src/MarvelComicsLibrary.Application/Controllers/ComicController.cs
@@ -4,122 +4,226 @@ using AutoMapper;
 using MarvelComicsLibrary.Application.Model;
 using MarvelComicsLibrary.Application.ViewModel;
 using MarvelComicsLibrary.Domain.Entity;
-using MarvelComicsLibrary.Domain.Entity.Values;
 using MarvelComicsLibrary.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MarvelComicsLibrary.Application.Controllers
 {
     /// <summary>
-    /// 
+    ///  API Controller for Comic Domain
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ComicController : ControllerBase
     {
-        private readonly IComicService _service;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private readonly IComicService _service;
+
         /// <summary>
-        /// 
+        /// Class Constructor
         /// </summary>
-        /// <param name="mapper"></param>
-        /// <param name="service"></param>
-        public ComicController(IMapper mapper, IComicService service)
+        /// <param name="mapper">AutoMapper</param>
+        /// <param name="logger">Serilog</param>
+        /// <param name="service">Comic Service</param>
+        public ComicController(IMapper mapper, ILogger logger, IComicService service)
         {
             _mapper = mapper;
+            _logger = logger;
             _service = service;
         }
 
+        /// <summary>
+        /// Fetch Comic List
+        /// </summary>
+        /// <returns type="ResponseRequest"></returns>
         [HttpGet]
         public ActionResult<List<ResponseRequest>> Get()
         {
-            var comicVM = _mapper.Map<List<ComicViewModel>>(_service.GetList());
+            try
+            {
+                _logger.LogInformation("GET Received");
 
-            return Ok(_mapper.Map<ResponseRequest>(comicVM));
+                var comicVM = _mapper.Map<List<ComicViewModel>>(_service.GetList());
+
+                return Ok(_mapper.Map<ResponseRequest>(comicVM));///
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
+            }
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Fecth a Comic by Key
+        /// </summary>
+        /// <param name="key">Comic ID</param>
         [HttpGet("{key}")]
         public ActionResult<ResponseRequest> Get(Guid key)
         {
-            var comicVM = _mapper.Map<ComicViewModel>(_service.Find(key));
+            try
+            {
+                _logger.LogInformation("Received GET");
 
-            return Ok(_mapper.Map<ResponseRequest>(comicVM));
+                var comicVM = _mapper.Map<ComicViewModel>(_service.Find(key));
+
+                return Ok(_mapper.Map<ResponseRequest>(comicVM));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
+            }
         }
 
-        // POST api/values
+        /// <summary>
+        ///  Create new Comic
+        /// </summary>
+        /// <param name="obj">ComicViewModel</param>
+        /// <returns>ResponseRequest</returns>
         [HttpPost]
         public ActionResult<ResponseRequest> Post([FromBody] ComicViewModel obj)
         {
-            var comic = _mapper.Map<Comic>(obj);
-
-            var save = _service.Add(comic);
-
-            var comicVM = _mapper.Map<ComicViewModel>(save);
-
-            if (!comicVM.Valid)
+            try
             {
-                return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
-            }
+                _logger.LogInformation("Received POST : {@data}", obj);
 
-            return Ok(_mapper.Map<ResponseRequest>(comicVM));
+                var comic = _mapper.Map<Comic>(obj);
+
+                var save = _service.Add(comic);
+
+                var comicVM = _mapper.Map<ComicViewModel>(save);
+
+                if (!comicVM.Valid)
+                {
+                    _logger.LogInformation("POST Invalid : {@data}", comicVM);
+
+                    return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
+                }
+
+                return Ok(_mapper.Map<ResponseRequest>(comicVM));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
+            }
         }
 
-        // PUT api/values/5
+        /// <summary>
+        /// Update a Comic by key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         [HttpPut("{key}")]
         public ActionResult<ResponseRequest> Put(Guid key, [FromBody] ComicViewModel obj)
         {
-            var dbComic = _service.Find(key);
-
-            if (dbComic == null)
+            try
             {
-                return BadRequest();
+                _logger.LogInformation("Received PUT : {@data}", obj);
+
+                var dbComic = _service.Find(key);
+
+                if (dbComic == null)
+                {
+                    _logger.LogInformation("Invalid PUT : {@data}", obj);
+
+                    return BadRequest("Comic Not Found");
+                }
+
+                var comic = _mapper.Map<Comic>(obj);
+
+                comic.Id = dbComic.Id;
+                comic.Key = key;
+
+                var update = _service.Amend(comic);
+
+                var comicVM = _mapper.Map<ComicViewModel>(update);
+
+                if (!comicVM.Valid)
+                {
+                    _logger.LogInformation("Invalid PUT : {@data}", obj);
+
+                    return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
+                }
+
+                return Ok(_mapper.Map<ResponseRequest>(comicVM));
             }
-
-            var comic = _mapper.Map<Comic>(obj);
-
-            comic.Id = dbComic.Id;
-            comic.Key = key;
-
-            var update = _service.Amend(comic);
-
-            var comicVM = _mapper.Map<ComicViewModel>(update);
-
-            if (!comicVM.Valid)
+            catch (Exception ex)
             {
-                return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
             }
-
-            return Ok(_mapper.Map<ResponseRequest>(comicVM));
         }
 
+        /// <summary>
+        /// Borrow a comic , set same attributes of Comic entity
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         [HttpPatch("Borrow")]
         public ActionResult<ResponseRequest> Patch(BorrowViewModel obj)
         {
-            var dbComic = _service.Find(obj.ComicKey);
-
-            if (dbComic == null)
+            try
             {
-                return BadRequest();
+                _logger.LogInformation("Received PATCH : {@data}", obj);
+
+                var dbComic = _service.Find(obj.ComicKey);
+
+                if (dbComic == null)
+                {
+                    _logger.LogInformation("Invalid PATCH :  Comic Not Found");
+                    return BadRequest("Comic Not Found");
+                }
+
+                var update = _service.BorrowComic(obj.CustomerKey, obj.ComicKey, obj.status);
+
+                var comicVM = _mapper.Map<ComicViewModel>(update);
+
+                if (!comicVM.Valid)
+                {
+                    return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
+                }
+
+                return Ok(_mapper.Map<ResponseRequest>(comicVM));
             }
-
-            var update = _service.BorrowComic(obj.CustomerKey, obj.ComicKey, obj.status);
-
-            var comicVM = _mapper.Map<ComicViewModel>(update);
-
-            if (!comicVM.Valid)
+            catch (Exception ex)
             {
-                return BadRequest(_mapper.Map<ResponseRequest>(comicVM));
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
             }
-
-            return Ok(_mapper.Map<ResponseRequest>(comicVM));
         }
 
-        // DELETE api/values/5
+        /// <summary>
+        /// Delete a comic
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         [HttpDelete("{key}")]
         public ActionResult Delete(Guid key)
         {
-            return Ok();
+            try
+            {
+                var dbComic = _service.Find(key);
+
+                if (dbComic == null)
+                {
+                    _logger.LogInformation("Invalid Delete :  Comic Not Found");
+                    return BadRequest("Comic Not Found");
+                }
+
+                _service.Remove(key);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
+            }
         }
     }
 }
---
+++ src/MarvelComicsLibrary.UnitTest/Application/ComicControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using GenFu;
+using MarvelComicsLibrary.Application.Controllers;
+using MarvelComicsLibrary.Application.ViewModel;
+using MarvelComicsLibrary.Domain.Entity;
+using MarvelComicsLibrary.Service.Interface;
+using MarvelComicsLibrary.UnitTest.Config;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace MarvelComicsLibrary.UnitTest.Application
+{
+    public class ComicControllerTest
+    {
+        
+        private readonly Mock<IComicService> _service;
+        private readonly Mock<ILogger<ComicController>> _logger;
+        private readonly ComicController _controller;
+
+        public ComicControllerTest()
+        {
+            _service = new Mock<IComicService>();
+            _logger = new Mock<ILogger<ComicController>>();
+            _controller = new ComicController( _logger.Object, AutoMapperConfigTest.GetInstance(),_service.Object);
+        }
+
+        [Fact(DisplayName = "Post Success")]
+        public void PostSuccess()
+        {
+            var borrowViewModel = A.New<BorrowViewModel>();
+            var borrowEntity = A.New<Borrow>();
+
+            _service.Setup(x => x.Add(It.IsAny<Borrow>())).Returns(borrowEntity);
+
+            var result = _controller.Post(borrowViewModel);
+
+            Assert.NotNull(result);
+
+            Assert.IsType<OkObjectResult>(result.Result);
+        }*/
+    }
+}
